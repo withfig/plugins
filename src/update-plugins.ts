2e2989e82a5d683b3d722b7920b6535e5779279f
Sean Sullivan <ssullivan61198@gmail.com>
2022-03-03 18:23:40 -0800

    use commonjs instead of esnext to support node backend

---
+++ src/update-plugins.ts
@@ -0,0 +1,17 @@
+import { loadPlugin } from ".";
+
+const pluginNames = process.argv.slice(2)
+  .filter(file => file.startsWith("src/plugins/") && file.endsWith(".ts"))
+  .map(file => file.slice("src/plugins/".length, -".ts".length));
+
+Promise.all(pluginNames.map(name => loadPlugin(name))).then(plugins => {
+  fetch("https://api.fig.io/plugins/update", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${process.env.PLUGINS_UPDATE_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ plugins })
+  }).then(res => process.exit(res.ok ? 0 : 1));
+})
+
