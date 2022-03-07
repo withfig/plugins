ca00b4a06f5e0f3c898251f173cd57b044a9bb3f
Sean Sullivan <ssullivan61198@gmail.com>
2022-03-07 15:48:56 -0800

    update index ts and add types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export const loadPlugin = async (name: string, forceLatest = false): Promise<Fig.Plugin> => {
+export const loadPlugin = async (name: string, forceLatest = false, esm = true): Promise<Fig.Plugin> => {
   const source = forceLatest
-    ? "https://cdn.jsdelivr.net/npm/@fig/plugins/build"
+    ? `https://cdn.jsdelivr.net/npm/@fig/plugins/dist/${esm ? "esm" : "cjs"}`
     : `.`;
   const mod = await import(`${source}/plugins/${name}.js`);
   return mod.default;
