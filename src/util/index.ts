c010eedb578045f7dae12c10897b4ea248cc5cba
Sean Sullivan <ssullivan61198@gmail.com>
2022-03-07 17:38:36 -0800

    move index into util

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,7 +1,7 @@
 export const loadPlugin = async (name: string, forceLatest = false, esm = true): Promise<Fig.Plugin> => {
   const source = forceLatest
     ? `https://cdn.jsdelivr.net/npm/@fig/plugins/dist/${esm ? "esm" : "cjs"}`
-    : `.`;
+    : `..`;
   const mod = await import(`${source}/plugins/${name}.js`);
   return mod.default;
 }
