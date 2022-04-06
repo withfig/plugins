43f9beeb16c3dd575ab568f65aa8b6f4dbbab279
Brendan Falk <brendan@fig.io>
2022-04-05 23:39:39 -0700

    zeno.zsh

--- plugins/zeno.zsh_yuki-yano/index.ts
+++ plugins/zeno.zsh_yuki-yano/index.ts
@@ -1,8 +1,13 @@
 const plugin: Fig.Plugin = {
   icon: "💾",
   name: "zeno.zsh_yuki-yano",
+  displayName: "zeno.zsh",
   type: "shell",
   description: "zsh fuzzy completion and utility plugin with Deno.",
+  screenshots: [
+    "https://user-images.githubusercontent.com/5423775/119225771-e0dfda80-bb40-11eb-8001-f5b575e29707.gif",
+    "https://user-images.githubusercontent.com/5423775/119226132-aaa35a80-bb42-11eb-9b90-1071fce1fc7d.gif",
+  ],
   authors: [
     {
       name: "yuki-yano",
