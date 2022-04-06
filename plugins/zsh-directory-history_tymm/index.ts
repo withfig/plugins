4b6859acbfc8881d362661ed3fb94939769d2590
Sean Sullivan <ssullivan61198@gmail.com>
2022-04-06 15:02:54 -0700

    moar plugins

--- plugins/zsh-directory-history_tymm/index.ts
+++ plugins/zsh-directory-history_tymm/index.ts
@@ -1,9 +1,9 @@
 const plugin: Fig.Plugin = {
   icon: "🧑‍💻",
+  displayName: "Directory History",
   name: "zsh-directory-history_tymm",
   type: "shell",
-  description:
-    "Giving you a history which is sensitive to the directory you are currently in",
+  description: "Provides directory-sensitive history features for zsh",
   authors: [
     {
       name: "tymm",
@@ -17,6 +17,44 @@ const plugin: Fig.Plugin = {
     origin: "github",
     sourceFiles: ["directory-history.plugin.zsh"],
   },
+  configuration: [
+    {
+      displayName: "History Search Forward Keybinding",
+      name: "history-search-forward-keybinding",
+      type: "script",
+      interface: "text",
+      default: "\\e[B",
+      compile: (value) =>
+        value ? `bindkey '${value}' directory-history-search-forward` : "",
+    },
+    {
+      displayName: "History Search Backward Keybinding",
+      name: "history-search-backward-keybinding",
+      type: "script",
+      interface: "text",
+      default: "\\e[A",
+      compile: (value) =>
+        value ? `bindkey '${value}' directory-history-search-backward` : "",
+    },
+    {
+      displayName: "Substring Search Up Keybinding",
+      name: "history-substring-search-up-keybinding",
+      type: "script",
+      interface: "text",
+      default: "^j",
+      compile: (value) =>
+        value ? `bindkey '${value}' history-substring-search-up` : "",
+    },
+    {
+      displayName: "Substring Search Down Keybinding",
+      name: "history-substring-search-down-keybinding",
+      type: "script",
+      interface: "text",
+      default: "^k",
+      compile: (value) =>
+        value ? `bindkey '${value}' history-substring-search-down` : "",
+    },
+  ],
 };
 
 export default plugin;
