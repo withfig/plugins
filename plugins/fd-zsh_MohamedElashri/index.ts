1ea499d6fadeeb578d31b253d7f12cd20cf2b3d5
Mohamed <muhammadelashri@gmail.com>
2022-11-20 15:48:57 -0500

    Fix additional space linting

--- plugins/fd-zsh_MohamedElashri/index.ts
+++ plugins/fd-zsh_MohamedElashri/index.ts
@@ -1,30 +1,30 @@
 const plugin: Fig.Plugin = {
-    icon: "⌨️",
-    name: "fd-zsh_MohamedElashri",
-    displayName: "fd aliases zsh plugin",
-    type: "shell",
-    description: "zsh plugin addind aliases for fd, a modern replacement for find.",
-    authors: [
+  icon: "⌨️",
+  name: "fd-zsh_MohamedElashri",
+  displayName: "fd aliases zsh plugin",
+  type: "shell",
+  description: "zsh plugin addind aliases for fd, a modern replacement for find.",
+  authors: [
+    {
+      name: "MohamedElashri",
+      github: "MohamedElashri",
+    },
+  ],
+  github: "MohamedElashri/fd-zsh/zsh-aliases-exa",
+  license: ["MIT"],
+  shells: ["zsh"],
+  categories: ["Alias"],
+  keywords: ["fd", "find", "zsh", "alias"],
+  installation: {
+    origin: "github",
+    sourceFiles: ["fd-zsh.plugin.zsh"],
+    dependencies: [
       {
-        name: "MohamedElashri",
-        github: "MohamedElashri",
+        type: "binary",
+        name: "fd",
       },
     ],
-    github: "MohamedElashri/fd-zsh/zsh-aliases-exa",
-    license: ["MIT"],
-    shells: ["zsh"],
-    categories: ["Alias"],
-    keywords: ["fd", "find", "zsh", "alias"],
-    installation: {
-      origin: "github",
-      sourceFiles: ["fd-zsh.plugin.zsh"],
-      dependencies: [
-        {
-          type: "binary",
-          name: "fd",
-        },
-      ],
-    },
-  };
+  },
+};
   
-  export default plugin;
+export default plugin;
